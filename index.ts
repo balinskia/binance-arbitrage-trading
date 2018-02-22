5968c53a880c59c7e16f2241c341811c9b088266
albertbalinski <albert.balinski@gmail.com>
2018-02-22 20:21:45 +0100

    Connect with binance

--- index.ts
+++ index.ts
@@ -6,5 +6,33 @@ import Currencies from './resources/currencies';
 import computeMaximumArbitrage from './triangular-arbitrage';
 import parseRates from './rates';
 
-const rates = parseRates(Pairs, Currencies);
-computeMaximumArbitrage(rates, Currencies);
+// Binance
+import BinanceKeys from './binance-keys';
+import * as Binance from './node_modules/node-binance-api/node-binance-api'
+
+const DEBUG = true;
+async function getPairs() {
+    return new Promise(function (resolve) {
+        if (DEBUG) {
+            // Return currency pairs from file
+            return resolve(Pairs);
+        } else {
+            // Return currency pairs from exchange
+            Binance.options({
+                APIKEY: BinanceKeys.APIKEY,
+                APISECRET: BinanceKeys.APISECRET,
+                useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
+                test: true // If you want to use sandbox mode where orders are simulated
+            });
+            Binance.prices((error, ticker) => {
+                resolve(ticker);
+            });
+        }
+    })
+}
+
+(async function start() {
+    const pairs = await getPairs();
+    const rates = parseRates(pairs, Currencies);
+    computeMaximumArbitrage(rates, Currencies);
+})()
--- secret.js
+++
@@ -1,4 +0,0 @@
-export default {
-  APIKEY: 'key',
-  APISECRET: 'secret',
-}
